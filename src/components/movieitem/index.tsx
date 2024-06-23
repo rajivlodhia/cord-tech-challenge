bdfe90c5f32d2d7e52fa0b4b0bac79f204ecd71c
rajivlodhia <r_lodhia@hotmail.com>
2024-06-23 21:38:21 +0100

    Styling the Movie Items

--- src/components/movieitem/index.tsx
+++ src/components/movieitem/index.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Movie } from "../../entities/Movie";
 import { buildImageLink } from "../../utils/image-link-builder";
+import * as colors from "../../colors";
 
 type MovieItemProps = {
     movie: Movie;
@@ -19,10 +20,14 @@ const MovieItem = ({ movie }: MovieItemProps) => {
             <RightCont>
                 <div className="movie-header">
                     <h2>{movie.title}</h2>
-                    <p>{movie.vote_average.toFixed(1)}</p>
+                    <p className="movie-rating">
+                        {movie.vote_average.toFixed(1)}
+                    </p>
                 </div>
-                <p>Action | Drama | Comedy</p>
-                <summary>{movie.overview}</summary>
+                <p className="movie-genres">Action | Drama | Comedy</p>
+                <summary className="movie-summary truncate">
+                    {movie.overview}
+                </summary>
                 <p className="movie-release-date">{movie.release_date}</p>
             </RightCont>
         </MovieItemWrapper>
@@ -40,6 +45,7 @@ const MovieItemWrapper = styled.article`
     margin-bottom: 15px;
     background-color: white;
     border-radius: 3px;
+    max-height: 270px;
 `;
 
 const LeftCont = styled.div`
@@ -53,13 +59,61 @@ const RightCont = styled.div`
     .movie-header {
         display: flex;
         justify-content: space-between;
+        align-items: center;
+        margin-bottom: 4px;
+    }
+
+    .movie-genres {
+        font-weight: 600;
+        font-size: 13px;
+        color: ${colors.primaryColor};
+        margin-bottom: 12px;
+    }
+
+    .movie-summary {
+        max-height: 125px;
     }
 
     .movie-release-date {
         margin: auto 0 0;
+        font-size: 13px;
+        color: ${colors.primaryColor};
+    }
+
+    .movie-rating {
+        line-height: normal;
+        height: fit-content;
+        padding: 4px 6px;
+        border-radius: 5px;
+        font-weight: 600;
+        color: #fff;
+        background-color: ${colors.primaryColor};
+    }
+
+    // Truncate the text and add ellipsis for the summary for anything more than 5 lines
+    // Adapted from: https://css-tricks.com/line-clampin/
+    .truncate {
+        display: -webkit-box;
+        -webkit-line-clamp: 5;
+        -webkit-box-orient: vertical;
+        overflow: hidden;
+        hyphens: auto;
+        text-align: left;
+
+        // Automatically use "word-break: break-all" for single-lines
+        // (https://css-tricks.com/css-switch-case-conditions)
+        --is-single-line: 1 - Clamp(0, Calc(5 - 1), 5);
+        --delay: Calc(-1s * (var(--is-single-line, 1) - 1));
+        animation: states 1s var(--delay) paused;
+
+        @keyframes states {
+            0% {
+                word-break: break-all;
+            }
+        }
     }
 `;
 
 const MoviePosterImage = styled.img`
-    max-width: 150px;
+    max-height: 225px;
 `;
